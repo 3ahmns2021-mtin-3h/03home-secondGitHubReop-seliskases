963060a14b09dea9235990e016b6992e33779cb7
simoneliskases <simon.eliskases@outlook.de>
2020-11-02 10:39:36 +0100

    Add Color Script

---
+++ Assets/MyGame/Scripts/Color.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Color : MonoBehaviour
+{
+    public GameObject colorPopUp;
+    public GameObject buttons;
+
+    private void Start()
+    {
+        ClosePopUp();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ClosePopUp();
+        }
+    }
+
+    public void OpenPopUp()
+    {
+        buttons.SetActive(false);
+        colorPopUp.SetActive(true);
+    }
+
+    public void ClosePopUp()
+    {
+        buttons.SetActive(true);
+        colorPopUp.SetActive(false);
+    }
+
+    public void ChangeColor(Image selectedImage)
+    {
+        Camera.main.backgroundColor = selectedImage.color;
+    }
+}
